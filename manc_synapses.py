0ad0c7efe14193d447392c5ea6f2304b89134481
Holly Whittome <hw666@cam.ac.uk>
2024-01-05 18:23:59 +0000

    changed from fetching all synapses and aggregating to just fetching connections with number of synapses as an attribute

--- manc_synapses.py
+++ manc_synapses.py
@@ -1,32 +1,30 @@
 import neuprint
 import os
 
-def fetch_downstream_synapses(neuron_id):
+def fetch_downstream_connections(neuron_id):
     client = neuprint.Client('neuprint.janelia.org', dataset='manc:v1.0')
-    synapses = neuprint.fetch_synapse_connections(source_criteria=neuron_id, batch_size=100)
-    syn = synapses[["bodyId_post", "x_pre", "y_pre", "z_pre"]].copy()
-    return syn
+    connections = neuprint.fetch_simple_connections([neuron_id])
+    synapse_values = connections[["bodyId_post", "weight"]].copy()
+    synapse_values = synapse_values.set_index("bodyId_post")
+    return synapse_values
 
-def fetch_upstream_synapses(neuron_id):
+def fetch_upstream_connections(neuron_id):
     client = neuprint.Client('neuprint.janelia.org', dataset='manc:v1.0')
-    synapses = neuprint.fetch_synapse_connections(target_criteria=neuron_id)
-    syn = synapses[["bodyId_pre", "x_pre", "y_pre", "z_pre"]].copy()
-    return syn
-
-def synapse_y_limit(synapses, y_limit):
-    return synapses[synapses["y_pre"]<y_limit]
+    connections = neuprint.fetch_simple_connections(None,[neuron_id])
+    synapse_values = connections[["bodyId_pre", "weight"]].copy()
+    synapse_values = synapse_values.set_index("bodyId_pre")
+    return synapse_values
 
 def get_value_counts(df):
     return df.iloc[:,0].value_counts()
 
 def downstream_of(neuron_id, N):
     #get synapses and sort value counts up to the Nth downstream synapse
-    downstream_synapses = fetch_downstream_synapses(neuron_id)
-    values = get_value_counts(downstream_synapses)
-    indices = values.head(N).index.to_list()
+    values = fetch_downstream_connections(neuron_id).head(N)
+    indices = values.index.to_list()
     for index in indices:
         print(index)
-    synapse_values = values.head(N).to_list()
+    synapse_values = values.weight.to_list()
     for synapse_value in synapse_values:
         print(synapse_value)
 
@@ -55,18 +53,15 @@ def cascade_csvs(neuron_id, N):
     downstream_neurons = make_csv(neuron_id, N)
     for neuron_id in downstream_neurons:
         print(neuron_id, end=" ")
-        if str(neuron_id)+"_downstreampartners.csv" in files:
+        if str(neuron_id)+"-downstreampartners.csv" in files:
             print("file already exists")
             pass
         else:
             make_csv(neuron_id, N)
             print("created file")
     
-
-
 def make_csv(neuron_id, N):
-    downstream_synapses = fetch_downstream_synapses(neuron_id)
-    values = get_value_counts(downstream_synapses).head(N)
+    values = fetch_downstream_connections(neuron_id).head(N)
     types = get_type(values.index.to_list())
     dataframe = types.merge(values, left_index=True, right_index=True)
     dataframe.to_csv(str(neuron_id)+"_downstreampartners.csv")
